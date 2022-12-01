aaf3c5bb8c0583d37bdbc5c3945ed148400c0257
AkaAugustoNeves <augusto.nevesti@gmail.com>
2022-12-01 19:45:17 -0300

    initialize project

---
+++ src/test/java/br/com/pilares/msperson/MsPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pilares.msperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
